64f17120242cffe659803c7a51d958bf9d26a12d
Adriano da Motta <45674915+adrianoslr@users.noreply.github.com>
2025-01-01 11:58:39 -0300

    Update data_processing.py
    Changed security options

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,24 +1,30 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, month, year
 
+# Configurar a segurança no Spark
 spark = SparkSession.builder \
     .appName("E-commerce Data Processing") \
     .config("spark.sql.warehouse.dir", "/tmp") \
     .config("spark.hadoop.fs.defaultFS", "file:///") \
     .config("spark.hadoop.fs.file.impl.disable.cache", "true") \
+    .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow") \
     .getOrCreate()
 
 # Iniciar sessão Spark
-spark = SparkSession.builder     .appName("E-commerce Data Processing")     .getOrCreate()
+spark = SparkSession.builder \
+    .config("spark.driver.host", "localhost") \
+    .appName("E-commerce Data Processing") \
+    .config("spark.driver.extraJavaOptions", "-Djava.security.manager=allow") \
+    .getOrCreate()
 
 # Carregar dados
-customers = spark.read.csv('../data/customers.csv', header=True, inferSchema=True)
-products = spark.read.csv('../data/products.csv', header=True, inferSchema=True)
-sales = spark.read.csv('../data/sales.csv', header=True, inferSchema=True)
+customers = spark.read.csv('./data/customers.csv', header=True, inferSchema=True)
+products = spark.read.csv('./data/products.csv', header=True, inferSchema=True)
+sales = spark.read.csv('./data/sales.csv', header=True, inferSchema=True)
 
 # Processar dados
 sales = sales.withColumn("sale_month", month(col("date")))
 sales = sales.withColumn("sale_year", year(col("date")))
 
 # Salvar os dados processados
-sales.write.csv('../data/processed_sales.csv', header=True, mode='overwrite')
+sales.write.csv('./data/processed_sales.csv', header=True, mode='overwrite')
